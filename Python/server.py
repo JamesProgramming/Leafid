c5e59ed7636e19507a5579a391155d35b781b1c9
James Coolidge <jamesjcoolidge@outlook.com>
2023-06-03 20:55:49 -0400

    Added comments to Python server

--- Python/server.py
+++ Python/server.py
@@ -1,40 +1,53 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, json
 import tensorflow as tf
 import urllib.parse
 from matplotlib.image import imread
-import math
 import os
 import flask_cors
 
+# Flask server.
 app = Flask(__name__)
 flask_cors.CORS(app)
 
+# Load pretrained model into program.
 loaded_model = tf.keras.models.load_model(
     os.path.join(os.path.dirname(__file__), "image_model")
 )
 
+# Load image info.
+# This file contains a list that correlates to the model output, mapping the indexes
+# of model output array indexes to the category names.
 with open(
     os.path.join(os.path.dirname(__file__), "image_info.json"), "r"
 ) as image_info:
     image_info = json.load(image_info)
 
 
+# Gets the models prediction of an image.
 @app.route("/api/predict", methods=["POST"])
 def index():
+    # Get image url and load image.
     url = urllib.parse.unquote(request.json["image"])
     test_image = imread(url)
+
+    # Reshape image. Expects (#, #, #, #);
     test_image = test_image.reshape((1, 256, 256, 3))
     results = (loaded_model.predict(test_image) * 100).tolist()[0]
 
+    # Get highest prediction.
     first_highest_percent = max(results)
     first_highest = results.index(first_highest_percent)
     results[first_highest] = -1
+
+    # Get second highest prediction.
     second_highest_percent = max(results)
     second_highest = results.index(second_highest_percent)
 
+    # Get predictions index values in the images_info file.
     map_first = image_info["map"][first_highest]
     map_second = image_info["map"][second_highest]
 
+    # Get each predction's full category name.
     category_first = (
         image_info["category"][map_first[0]]["name"]
         + " "
@@ -62,6 +75,7 @@ def index():
     )
 
 
+# Load model infomation files.
 with open(
     os.path.join(os.path.dirname(__file__), "history.json"), "r"
 ) as model_history:
@@ -78,6 +92,7 @@ with open(
     test_results = json.load(test_results)
 
 
+# Returns infomation about model training history and dataset.
 @app.route("/api/stats", methods=["GET"])
 def imageStats():
     return (
@@ -94,4 +109,5 @@ def imageStats():
     )
 
 
-# app.run(port=3002, debug=True)
+if __name__ == "__main__":
+    app.run(port=3002, debug=True)
